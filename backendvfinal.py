4f9607e936e4cffea7f9fb4321e7d528ebe3728f
severlove <48201655+severlove@users.noreply.github.com>
2019-03-06 12:30:40 -0500

    Add files via upload

---
+++ backendvfinal.py
@@ -0,0 +1,67 @@
+ #!/usr/bin/env python3
+import psycopg2
+
+
+def top_3_articles():
+    '''Print top three articles of all time.'''
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    query = """SELECT articles.title, count(articles.title) as num
+               FROM articles,log
+               WHERE articles.slug = SUBSTRING(log.path, 10)
+               GROUP BY title
+               ORDER BY num DESC
+               LIMIT 3;"""
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    print ("\nTop 3 Articles:\n")
+    for i in range(len(results)):
+        print "\"" + results[i][0] + "\" - " + str(results[i][1]) + " views"
+
+
+def popular_authors():
+    '''Print the most popular authors of all time.'''
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    query = """SELECT authors.name, count(articles.author) as num
+               FROM articles, log, authors
+               WHERE articles.slug = SUBSTRING(log.path, 10) and
+               articles.author = authors.id
+               GROUP BY authors.name
+               ORDER BY num DESC"""
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    print ("\nPopular Authors:\n")
+    for i in range(len(results)):
+        print results[i][0] + " - " + str(results[i][1]) + " views"
+
+
+def errors():
+    '''Print days where more than 1% of requests lead to errors.'''
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    query = """SELECT Date, Total, Error,
+               (Error::float*100)/Total::float as Percent
+               FROM (
+               SELECT to_char(time::timestamp::date, 'Month DD, YYYY') as Date,
+               count(status) as Total, sum(case when not status =
+               '200 OK' then 1 else 0 end) as Error
+               FROM log
+               GROUP BY time::timestamp::date) as result
+               WHERE (Error::float*100)/Total::float > 1.0;"""
+    c.execute(query)
+    results = c.fetchall()
+    db.close()
+    print ("\nDays where more than 1% of requests lead to errors:\n")
+    for i in range(len(results)):
+        print(str(results[i][0]) +
+              " - " + str(round(results[i][3], 1)) + "% errors")
+    print ("\n")
+
+
+if __name__ == '__main__':
+    top_3_articles()
+    popular_authors()
+    errors()
